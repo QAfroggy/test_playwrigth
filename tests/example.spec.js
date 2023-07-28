f7faff33ac725aff40cad8ea95fe4226883c885e
Murat Karaiev <qa.froggy@gmail.com>
2023-07-28 12:16:26 +0300

    BBC 1 test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,18 +2,21 @@
 const { test, expect } = require('@playwright/test');
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://www.bbc.com/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle("BBC - Homepage");
+  const logo = await page.locator('#homepage-link');
+  await expect(logo).toBeVisible();
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// test('get started link', async ({ page }) => {
+//   //await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+//   // Click the get started link.
+//   await expect( page.getByAltText("BBC Logo")).toBeVisible();
+//   //await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
-});
+//   // Expects the URL to contain intro.
+//   //await expect(page).toHaveURL(/.*intro/);
+// });
