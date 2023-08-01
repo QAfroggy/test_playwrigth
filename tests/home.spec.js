ed2f8891de4287081c5569c12d114b87b4e36c60
Murat Karaiev <qa.froggy@gmail.com>
2023-08-01 18:16:43 +0300

    total fix

--- tests/home.spec.js
+++ tests/home.spec.js
@@ -1,79 +1,25 @@
 import { test, expect } from "@playwright/test";
-import {HomePage} from '../utils/pages/home';
+import { HomePage } from "../utils/pages/home";
+
+test.describe("Describe elements on the website", ()=>{
 
 
-  
-
-  test.beforeEach(async ({page}) => {
-    const Home = new HomePage(page)
-    await Home.openPage()
-    //await page.goto("https://www.bbc.com/");
-  });
-  test("After open url page is visible", async ({ page }) => {
-    const Home = new HomePage(page)
-      await expect(Home.homePage).toBeVisible()
-  });
-  // test("Home page has title BBC -Homepage", async ({ page }) => {
-    
-  //   await expect(page).toHaveTitle("BBC - Homepage");
-  // });
-  // test("Logo BBC is visible", async ({ page }) => {
-    
-  //   const logo = page.locator("#homepage-link");
-  //   await expect(logo).toBeVisible();
-  // });
-
-  // test("Let us know you agree to advertising cookies", async ({ page }) => {
-    
-  //   const advertisingCookies = page
-  //     .frameLocator('iframe[title="SP Consent Message"]')
-  //     .getByLabel("I agree");
-  //   await expect(advertisingCookies).toBeVisible();
-  //   await page
-  //     .frameLocator('iframe[title="SP Consent Message"]')
-  //     .getByLabel("I agree")
-  //     .click();
+test.beforeEach(async ({ page }) => {
+  const Home = new HomePage(page);
+  await Home.openPage();
  
-  // });
-  // test('Search BBC is visible', async({page})=>{
-   
-  //  const searchBBC= page.locator('[title="Search BBC"]');
-  //  await expect(searchBBC).toBeVisible();
-  // });
+});
+
+test("After open url page is visible", async ({ page }) => {
+  const Home = new HomePage(page);
+  await expect(Home.homePage).toBeVisible();
+});
+
+test("Logo BBC is visible", async ({ page }) => {
+  const Home = new HomePage(page);
+  await expect(Home.logo_bbc).toBeVisible();
+});
+
+});
 
 
-
-
-
-
-
-// test('has title', async ({ page }) => {
-
-//   // Expect a title "to contain" a substring.
-//   //await expect(page).toHaveTitle("BBC - Homepage");
-//   await expect(page).toHaveTitle(Home.expected.title);
-
-// });
-// test('The logo is visible', async ({ page }) => {
-
-//  const logo = page.locator(Home.selectors.logo);
-//  await expect(logo).toBeVisible();
-
-// });
-
-// test('The link is https://www.bbc.com', async ({ page }) => {
-//  //Page has link https://www.bbc.com
-//   await expect(page).toHaveURL(Home.expected.baseUrl);
-
-// });
-// test('Cookie popup is visible', async ({ page }) => {
-//  const cookie = page.locator(Home.selectors.cookie);
-//  await expect(cookie).toBeVisible();
-
-// });
-// test('Sign In is visible', async ({ page }) => {
-//   const signIn = page.locator(Home.selectors.signIn);
-//   await expect(signIn).toBeVisible();
-
-//  });
-// });
