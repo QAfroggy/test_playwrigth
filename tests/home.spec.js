20231ca0c5cce09e0d07172bc7b459da87faa3f7
Murat Karaiev <qa.froggy@gmail.com>
2023-08-01 17:55:48 +0300

    BIG fix

--- tests/home.spec.js
+++ tests/home.spec.js
@@ -1,51 +1,51 @@
 import { test, expect } from "@playwright/test";
+import {HomePage} from '../utils/pages/home';
 
-test.describe("Elements ", async () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto("https://www.bbc.com/");
+
+  
+
+  test.beforeEach(async ({page}) => {
+    const Home = new HomePage(page)
+    await Home.openPage()
+    //await page.goto("https://www.bbc.com/");
   });
   test("After open url page is visible", async ({ page }) => {
-    
-    const homePage = page.locator("#page");
-    await expect(homePage).toBeVisible();
+    const Home = new HomePage(page)
+      await expect(Home.homePage).toBeVisible()
   });
-  test("Home page has title BBC -Homepage", async ({ page }) => {
+  // test("Home page has title BBC -Homepage", async ({ page }) => {
     
-    await expect(page).toHaveTitle("BBC - Homepage");
-  });
-  test("Logo BBC is visible", async ({ page }) => {
+  //   await expect(page).toHaveTitle("BBC - Homepage");
+  // });
+  // test("Logo BBC is visible", async ({ page }) => {
     
-    const logo = page.locator("#homepage-link");
-    await expect(logo).toBeVisible();
-  });
+  //   const logo = page.locator("#homepage-link");
+  //   await expect(logo).toBeVisible();
+  // });
 
-  test("Let us know you agree to advertising cookies", async ({ page }) => {
+  // test("Let us know you agree to advertising cookies", async ({ page }) => {
     
-    const advertisingCookies = page
-      .frameLocator('iframe[title="SP Consent Message"]')
-      .getByLabel("I agree");
-    await expect(advertisingCookies).toBeVisible();
-    await page
-      .frameLocator('iframe[title="SP Consent Message"]')
-      .getByLabel("I agree")
-      .click();
-    // await page
-    //   .getByLabel("BBC-wide")
-    //   .getByRole("link", { name: "News" })
-    //   .click();
-    //await page.getByRole('link', { name: 'Sign in' }).click();
-    // await page.getByLabel('Email or username').click();
-    // await page.getByLabel('Email or username').fill('jdjdj');
-    // await page.getByLabel('Password').click();
-    // await page.getByLabel('Password').fill('d,d,d,smd,');
-    // await page.getByRole('button', { name: 'Sign in' }).click();
-  });
-  test('Search BBC is visible', async({page})=>{
+  //   const advertisingCookies = page
+  //     .frameLocator('iframe[title="SP Consent Message"]')
+  //     .getByLabel("I agree");
+  //   await expect(advertisingCookies).toBeVisible();
+  //   await page
+  //     .frameLocator('iframe[title="SP Consent Message"]')
+  //     .getByLabel("I agree")
+  //     .click();
+ 
+  // });
+  // test('Search BBC is visible', async({page})=>{
    
-   const searchBBC= page.locator('[title="Search BBC"]');
-   await expect(searchBBC).toBeVisible();
-  });
-});
+  //  const searchBBC= page.locator('[title="Search BBC"]');
+  //  await expect(searchBBC).toBeVisible();
+  // });
+
+
+
+
+
+
 
 // test('has title', async ({ page }) => {
 
--- utils/pages/home.js
+++ utils/pages/home.js
@@ -1,68 +1,20 @@
-// // //selectors
-// // const selectors ={
-// //     logo: '#homepage-link',
-// //     url: 'https://www.bbc.com/',
-// //     cookie: ".bbccookies-banner",
-// //     headerNav: '.orbit-header-links international',
-// //     signIn: '#idcta-username'
+exports.HomePage = class HomePage {
+  constructor(page) {
+    this.page = page;
+    this.homePage = page.locator("#page");
+    this.logo_bbc = page.locator("#homepage-link");
+    this.cookie = page
+      .frameLocator('iframe[title="SP Consent Message"]')
+      .getByLabel("I agree");
+    this.search = page.locator('[title="Search BBC"]');
+  }
 
-// // }
-
-
-
-
-
-// // //expected
-// // const expected ={
-// //     title: "BBC - Homepage",
-// //     baseUrl: 'https://www.bbc.com/'
-
-// // };
-// const { expect } = require('@playwright/test');
-
-// exports.Home = class Home {
-
-// //   /**
-// //    * @param {import('@playwright/test').Page} page
-// //    */
-//   constructor(page) {
-//     this.page = page;
-//     this.logo = page.locator('#homepage-link');
-//     this.cookie = page.locator(".bbccookies-banner");
-//     this.headerNav = page.locator('.orbit-header-links international',);
-//     this.signIn = page.locator('#idcta-username');
-//   }
-
-//   async goto() {
-//     await this.page.goto('https://www.bbc.com');
-//   }
-
-//   async getStarted() {
-//     await this.getStartedLink.first().click();
-//     await expect(this.gettingStartedHeader).toBeVisible();
-//   }
-
-//   async pageObjectModel() {
-//     await this.getStarted();
-//     await this.pomLink.click();
-//   }
-// };
+ async clickIAgree(){
+await this.cookie.click()
+ }
+ async openPage(){
+    await this.page.goto("https://www.bbc.com/");
+ }
 
 
-
-
-
-
-// //functions
-
-// const openPage = async() =>{
-//     await page.goto('https://www.bbc.com/');
-// }
-
-// export default{
-//     selectors,
-//     expected,
-//     openPage
-// }
-
-
+}
